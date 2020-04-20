3f5b9935d0798f9983afab0bb1f39fb5b68b7d0d
fronox <serjzv@gmail.com>
2020-04-20 20:47:48 +0300

    Async functions are added

--- lib/agent_example.ex
+++ lib/agent_example.ex
@@ -1,15 +1,13 @@
 defmodule AgentExample do
-  use Agent
-
   def agent_name do
     __MODULE__
   end
 
-  def start_link({:initial, state}) when is_map(state) do
+  def start({:initial, state}) when is_map(state) do
     Agent.start_link(fn -> state end, name: agent_name())
   end
 
-  def start_link() do
+  def start() do
     Agent.start_link(fn -> %{} end, name: agent_name())
   end
 
@@ -25,18 +23,28 @@ defmodule AgentExample do
     Agent.update(agent_name(), fn state -> state |> Map.put(key, value) end)
   end
 
+  def add_async(key, value) do
+    Agent.cast(agent_name(), fn state -> state |> Map.put(key, value) end)
+  end
+
   def remove({:key, key}) do
     Agent.update(agent_name(), fn state -> state |> Map.delete(key) end)
   end
 
+  def remove_async({:key, key}) do
+    Agent.cast(agent_name(), fn state -> state |> Map.delete(key) end)
+  end
+
   def pop({:key, key}) do
-    {result, new_state} = Agent.get(agent_name(), & Map.pop(&1, key))
-    Agent.update(agent_name(), fn _ -> new_state end)
-    result
+    Agent.get_and_update(agent_name(), fn state -> state |> Map.pop(key) end)
   end
 
   def clean() do
     Agent.update(agent_name(), fn _ -> %{} end)
+  end
+
+  def clean_async() do
+    Agent.cast(agent_name(), fn _ -> %{} end)
   end
 
   def stop() do
