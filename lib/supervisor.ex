15c0bd534fc0759dce46e098a823b4311e9ef17c
fronox <serjzv@gmail.com>
2020-04-27 22:56:27 +0300

    Supervisor draft is created

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -1,3 +1,13 @@
 defmodule SupExample do
-  # TODO
+
+  def start_gen_server() do
+    children = [
+      %{
+        id: GSExample,
+        start: {GSExample, :start_link, [%{}]}
+      }
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
 end
